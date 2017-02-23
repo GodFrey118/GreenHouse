61a47a4b06b467352d515792369f1848772b0b74
vovol <vovol@126.com>
2017-02-23 18:54:38 +0800

    Signed-off-by: vovol <vovol@126.com>

--- GreenHouse/src/main/java/com/yc/GreenHouse/web/handler/UserHandler.java
+++ GreenHouse/src/main/java/com/yc/GreenHouse/web/handler/UserHandler.java
@@ -3,13 +3,11 @@ package com.yc.GreenHouse.web.handler;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
