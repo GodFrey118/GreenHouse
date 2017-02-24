34f60cdfead383a321f017f9468abe3e0353398f
HeJinRongq <568839197@qq.com>
2017-02-24 12:47:02 +0800

    购物车的部分代码 ---hjr

--- GreenHouse/src/main/java/com/yc/GreenHouse/mapper/StoreMapper.java
+++ GreenHouse/src/main/java/com/yc/GreenHouse/mapper/StoreMapper.java
@@ -3,6 +3,7 @@ package com.yc.GreenHouse.mapper;
 import java.util.List;
 
 import com.yc.GreenHouse.entity.Good;
+import com.yc.GreenHouse.entity.Shoping_Cart;
 import com.yc.GreenHouse.entity.Store;
 
 public interface StoreMapper {
@@ -14,5 +15,7 @@ public interface StoreMapper {
 	Good selectgoodInfo(int g_id);
 
 	List<Good> selectGoodssaInfo(int s_id);
+
+	int indsertCart(Shoping_Cart sCart);
 
 }
