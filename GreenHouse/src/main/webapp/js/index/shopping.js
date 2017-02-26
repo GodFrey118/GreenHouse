b46a4373f7c023200a1c116b713f01d7fa679b8c
HeJinRongq <568839197@qq.com>
2017-02-26 15:41:07 +0800

    购物车

--- GreenHouse/src/main/webapp/js/index/shopping.js
+++ GreenHouse/src/main/webapp/js/index/shopping.js
@@ -3,7 +3,6 @@ $.post("user/cartNum",function(data){
 		
 			  for(var i=0;i<data.length;i++){
 				  for(var j=0;j<data[i].goods.length;j++){
-					  alert(data[i].g_id+"内");
 					  $("#shopping").append(
 								"<tr class='shop'>"
 								+"<td colspan='2'><input type='checkbox' id='1898538' class='J_forShop J_MakePoint store_sel' onclick='store_select(this);' tag='22' checked='checked'>"
@@ -26,7 +25,7 @@ $.post("user/cartNum",function(data){
 								+"<span>保障: </span> <a class='xb-' target='_blank' href='#'></a></div></div></td>"
 								+"<td class='s-point'><em><span id='item177493_goods_integral' tag='22'>22</span></em></td>"
 								+"<td class='s-price '><del>¥40.00</del><em id='price'>¥" + parseFloat((data[i].goods)[j].g_price).toFixed(2) + "</em><span>省¥18.00元</span></td>"
-								+"<td class='s-amount '><img src='./shopping_files/subtract.gif' onClick='decrease("+i+","+data[i].g_id+");' width='12' height='12'><input id='a"+i+"' value='"+ data[i].sc_goodNum +"' class='text1 width3' type='text'/>"
+								+"<td class='s-amount '><img src='./shopping_files/subtract.gif' onClick='decrease("+i+","+data[i].g_id+");' width='12' height='12'><input id='a"+i+"' value='"+ data[i].sc_goodNum +"' onfocus='chkNum("+i+",this,"+data[i].g_id+")' onblur='chkvalue("+i+",this,"+data[i].g_id+")' class='text1 width3' type='text'/>"
 								+"<img src='./shopping_files/adding.gif' onClick='add("+i+","+ data[i].sc_goodNum +","+data[i].g_id+");' width='12' height='12'></td>"
 								+"<td class='s-agio'><span id='discountList_177493'>"
 								+"<span class='m-promo-item'> <a href='javascript:viod(0)' active='卖家促销' ever_dif='18' title='省18元：卖家促销...'><img src='./shopping_files/cuxiao.png'></a></span>"
@@ -39,20 +38,20 @@ $.post("user/cartNum",function(data){
 					  
 				  }
 				  total= total + parseFloat(($("#"+i).text()).substr(1, ($("#"+i).text()).length));
-				  alert($("#a"+i).val() + "sd");
 			  }
 			  
-			  alert(parseFloat(total).toFixed(2));
 			  $("#cart_amount").html("￥"+parseFloat(total).toFixed(2));
 		  
 	},'json');
+
+
+ 
 	
 	
    function	drop_cart(sc_id,i,dele){
 	   $.post("user/delCartGood?sc_id="+sc_id,function(data){
 			if(data){
 				alert("确定要删除？");
-				
 				var price_1=parseFloat(($("#"+i).text()).substr(1, ($("#"+i).text()).length));
 				var total_2 = parseFloat(parseFloat(($("#cart_amount").text()).substr(1, ($("#cart_amount").text()).length))-price_1).toFixed(2);
 				$("#cart_amount").html("￥"+total_2);
@@ -64,34 +63,20 @@ $.post("user/cartNum",function(data){
    }
 	
 	
-   /*function add(i,g_id){
-		
-
-	$.post("user/goodNum?sc_id="+g_id,function(data){
-			
-		},"json");
-	}*/
 	var a=0;
+	var b=0.00;
 	function decrease(i,g_id){
 		var sc_goodNum=$("#a"+i).val();
 		a=sc_goodNum-1;
 		if(sc_goodNum>1){
-		
-		alert(a);
-		alert(g_id);
 		$("#a"+i).val(a);
-		
-		//$("#a1").val();
-		
 		$.post("user/goodNum?g_id="+g_id+"&sc_goodNum="+(a),function(data){
-			alert(data);
 			if(data){
 				var single_price=parseFloat(($("#price").text()).substr(1, ($("#price").text()).length));
-				alert(single_price+"单价");
 				var total_price=parseFloat((a)*single_price).toFixed(2);
-				alert(total_price+"总价");
 				$("#"+i).html("￥"+total_price);
-				$("#cart_amount").html("￥"+parseFloat(($("#cart_amount").text()).substr(1, ($("#cart_amount").text()).length)-single_price).toFixed(2));
+				b=parseFloat(($("#cart_amount").text()).substr(1, ($("#cart_amount").text()).length)-single_price).toFixed(2);
+				$("#cart_amount").html("￥"+b);
 			}
 		},"json");
 		
@@ -102,22 +87,50 @@ $.post("user/cartNum",function(data){
 	function add(i,sc_goodNum,g_id){
 		var sc_goodNum=$("#a"+i).val();
 		a=parseInt(sc_goodNum)+1;
-		alert(a);
-		alert(g_id);
 		$("#a"+i).val(a);
-		
-		//$("#a1").val();
-		
 		$.post("user/goodNum?g_id="+g_id+"&sc_goodNum="+(a),function(data){
-			alert(data);
 			if(data){
 				var single_price=parseFloat(($("#price").text()).substr(1, ($("#price").text()).length));
 				alert(single_price+"单价");
 				var total_price=parseFloat((a)*single_price).toFixed(2);
 				alert(total_price+"总价");
 				$("#"+i).html("￥"+total_price);
-				$("#cart_amount").html("￥"+parseFloat(($("#cart_amount").text()).substr(1, ($("#cart_amount").text()).length)+single_price).toFixed(2));
+				var s =parseFloat(parseInt(($("#cart_amount").text()).substr(1, ($("#cart_amount").text()).length)) + parseInt(single_price)).toFixed(2) ;
+				alert(s);
+				$("#cart_amount").html("￥"+s);
 			}
 		},"json");
 		
 	}
+	 
+	var num=0;
+	var total_price1=0;
+	
+	  function chkNum(i){
+		  num= $("#a"+i).val()
+		  var single_price=parseFloat(($("#price").text()).substr(1, ($("#price").text()).length));
+		  total_price1=parseFloat(($("#a"+i).val())*single_price).toFixed(2);
+		
+	   }
+	  
+	
+	 function chkvalue(j,num,g_id){
+		 if($("#a"+j).val()>1){
+		 $.post("user/goodNum?g_id="+g_id+"&sc_goodNum="+($("#a"+j).val()),function(data){
+			 if(data){
+		 var single_price=parseFloat(($("#price").text()).substr(1, ($("#price").text()).length));
+		 var total_price=parseFloat(($("#a"+j).val())*single_price).toFixed(2);
+		 alert(total_price +"小计价" + total_price1);
+		 $("#"+j).html("￥"+ total_price );
+		 var f = (parseInt(total_price)-parseInt(total_price1));
+		 alert(f);
+		 var s =parseFloat(parseInt(($("#cart_amount").text()).substr(1, ($("#cart_amount").text()).length)) + (parseInt(total_price)-parseInt(total_price1))).toFixed(2);
+		 $("#cart_amount").html("￥"+s);
+			 }
+		 },"json");
+		 
+		 }
+	}
+  
+		 
+		
